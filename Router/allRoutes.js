048d95e3d785eaae2415747f64e8ded7038a898f
Ashura1234 <156065272+Ashura1234@users.noreply.github.com>
2025-02-20 12:00:56 +0100

    add content page, reservations, creations, connexion, inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,14 @@ import Route from "./route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/accueil.html"),
     new Route("404", "Page introuvable", "/pages/404.html"),
+    new Route("/connexion", "connexion", "/pages/functions/connexion.html"),
+    new Route("/inscription", "inscription", "/pages/functions/inscription.html"),
+    new Route("/deconnexion", "déconnexion", "/pages/functions/deconnexion.html"),
+    new Route("/itineraire", "itinéraire", "/pages/services/itineraire.html"),
+    new Route ("/reservations", "mes réservations", "/pages/services/reservations.html"),
+    new Route ("/contact", "Nous contacter", "/pages/services/contact.html"),
+    new Route ("/creer", "créer un trajet", "/pages/services/creationTrajet.html"),
+    new Route ("/mesTrajets", "mes Trajets", "/pages/services/mesTrajets.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
